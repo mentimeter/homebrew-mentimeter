a51601afa4782697aee222737ea2976a10bbdc9c
Oliver Stenbom <oliver@stenbom.eu>
2023-08-18 09:11:40 +0200

    Bump linkup to 0.0.13

--- linkup.rb
+++ linkup.rb
@@ -4,11 +4,11 @@ class Linkup < Formula
   license "Apache-2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/mentimeter/linkup/releases/download/0.0.12/linkup-0.0.12-aarch64-apple-darwin.tar.gz"
-    sha256 "139d4f449384aeb259ada51ce739dd504c0728d1a287ec71696223bb6e4ab4e5"
+    url "https://github.com/mentimeter/linkup/releases/download/0.0.13/linkup-0.0.13-aarch64-apple-darwin.tar.gz"
+    sha256 "1342b182f8a642d4fa9e7b08d633a5f9db4ecbd55eb751cc9c4deb884e34fc14"
   elsif Hardware::CPU.intel?
-    url "https://github.com/mentimeter/linkup/releases/download/0.0.12/linkup-0.0.12-x86_64-apple-darwin.tar.gz"
-    sha256 "6776cd11a83616f71f4bf40ad7cab84102e6f82d797f09d936d3a390d73cdbdf"
+    url "https://github.com/mentimeter/linkup/releases/download/0.0.13/linkup-0.0.13-x86_64-apple-darwin.tar.gz"
+    sha256 "f8d1149e16dc6bfa07560615cc078690ce272971a8d141249af5824939731582"
   end
 
   depends_on "cloudflared"
