1d41c169709f380f8de08c78488975a4ca2f23e9
Oliver Stenbom <oliver@stenbom.eu>
2023-11-03 10:55:02 +0100

    Fix sha copy-paste error

--- linkup.rb
+++ linkup.rb
@@ -8,7 +8,7 @@ class Linkup < Formula
     sha256 "8fc791ae127f2bceb0f63de8da75ad468185808a76672b044d5c75d6d58d9cbe"
   elsif Hardware::CPU.intel?
     url "https://github.com/mentimeter/linkup/releases/download/0.1.13/linkup-0.1.13-x86_64-apple-darwin.tar.gz"
-    sha256 "8fc791ae127f2bceb0f63de8da75ad468185808a76672b044d5c75d6d58d9cbe"
+    sha256 "f570b129f6707a81335a6cfca98b6b37132d307ad7adb2eaef675c46c69e1e28"
   end
 
   depends_on "cloudflared"
