53ef76e2959a13eae3b557e591eb05bb0c27fb39
Oliver Stenbom <oliver@stenbom.eu>
2023-11-08 14:53:58 +0100

    Move URLs to consts

--- linkup.rb
+++ linkup.rb
@@ -1,14 +1,19 @@
+ARM_URL "https://github.com/mentimeter/linkup/releases/download/0.1.14/linkup-0.1.14-aarch64-apple-darwin.tar.gz"
+ARM_SHA "384c1d461a34419456910cca2d1ef25ffe9288de583672c5a55fff48d80c009e"
+X86_URL "https://github.com/mentimeter/linkup/releases/download/0.1.14/linkup-0.1.14-x86_64-apple-darwin.tar.gz"
+X86_SHA "32fa5e6b651a08425955094e4769fcf371f9218c51738656998935754a938d8c"
+
 class Linkup < Formula
   desc "Link up preview environments"
   homepage "https://github.com/mentimeter/linkup"
   license "Apache-2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/mentimeter/linkup/releases/download/0.1.14/linkup-0.1.14-aarch64-apple-darwin.tar.gz"
-    sha256 "384c1d461a34419456910cca2d1ef25ffe9288de583672c5a55fff48d80c009e"
+    url ARM_URL
+    sha256 ARM_SHA
   elsif Hardware::CPU.intel?
-    url "https://github.com/mentimeter/linkup/releases/download/0.1.14/linkup-0.1.14-x86_64-apple-darwin.tar.gz"
-    sha256 "32fa5e6b651a08425955094e4769fcf371f9218c51738656998935754a938d8c"
+    url X86_URL
+    sha256 X86_SHA
   end
 
   depends_on "cloudflared"
