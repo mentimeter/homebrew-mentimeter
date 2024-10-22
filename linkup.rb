15fea3b28e2ebf7536675c95e9d8dc7ff81077f7
mentibot <122382604+mentibot@users.noreply.github.com>
2024-10-22 07:28:25 +0000

    Update Linkup formula to 1.0.4

--- linkup.rb
+++ linkup.rb
@@ -1,7 +1,7 @@
 ARM_URL = "https://github.com/mentimeter/linkup/releases/download/1.0.4/linkup-1.0.4-aarch64-apple-darwin.tar.gz"
-ARM_SHA = "64e55075dd18be59daa969b2258e0c1fae61f7c42e65a9897fc726a57cdfdd92"
+ARM_SHA = "0f5bf2ddd7d0b8afaaf12b2347a1402f3c2395a20478f19db33fd02b7facbfb8"
 X86_URL = "https://github.com/mentimeter/linkup/releases/download/1.0.4/linkup-1.0.4-x86_64-apple-darwin.tar.gz"
-X86_SHA = "9db8cbd21039040c47841dd3aa2b21d03a489728edb480741d4341cedf17442b"
+X86_SHA = "c86742fffdbcd0f47a28a5f4108dc010c4367c3e6eb2a1ab789c5590ce5b9de1"
 
 class Linkup < Formula
   desc "Link up preview environments"
