c8aafefce96e26ef80c2f84dd0aad3f82f760135
Augusto César <augusto.silva@mentimeter.com>
2025-01-07 14:17:54 +0100

    feat: show Linux assets as well

--- show-latest-linkup.sh
+++ show-latest-linkup.sh
@@ -19,3 +19,21 @@ MAC_X86_SHA=$(curl -L -s $MAC_X86_SHA_LINK)
 printf "Latest Mac x86 Linkup:\n"
 printf "%s\n" $MAC_X86_LINK
 printf "%s\n" $MAC_X86_SHA
+
+printf "\n"
+
+LINUX_ARM_SHA_LINK=$(echo $ASSETS | jq -r 'select(.name | test("aarch64-unknown-linux-gnu.tar.gz.sha256$")) | .browser_download_url')
+LINUX_ARM_LINK=$(echo $ASSETS | jq -r 'select(.name | test("aarch64-unknown-linux-gnu.tar.gz$")) | .browser_download_url')
+LINUX_ARM_SHA=$(curl -L -s $LINUX_ARM_SHA_LINK)
+printf "Latest Linux ARM Linkup:\n"
+printf "%s\n" $LINUX_ARM_LINK
+printf "%s\n" $LINUX_ARM_SHA
+
+printf "\n"
+
+LINUX_X86_SHA_LINK=$(echo $ASSETS | jq -r 'select(.name | test("x86_64-unknown-linux-gnu.tar.gz.sha256$")) | .browser_download_url')
+LINUX_X86_LINK=$(echo $ASSETS | jq -r 'select(.name | test("x86_64-unknown-linux-gnu.tar.gz$")) | .browser_download_url')
+LINUX_X86_SHA=$(curl -L -s $LINUX_X86_SHA_LINK)
+printf "Latest Linux x86 Linkup:\n"
+printf "%s\n" $LINUX_X86_LINK
+printf "%s\n" $LINUX_X86_SHA
