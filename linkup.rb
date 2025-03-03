ab874232c88f322e5c91cd6c3e6b633b480252d3
Augusto César <augusto.silva@mentimeter.com>
2025-03-03 10:45:31 +0100

    feat(wip): connect homebrew to GitHub release

--- linkup.rb
+++ linkup.rb
@@ -1,44 +1,65 @@
-MAC_ARM_URL = "https://github.com/mentimeter/linkup/releases/download/2.2.0/linkup-2.2.0-aarch64-apple-darwin.tar.gz"
-MAC_ARM_SHA = "32f70f4884ec337109446701632ffef859f77d53cb794671b5784dd7f5cfa0b1"
-MAC_X86_URL = "https://github.com/mentimeter/linkup/releases/download/2.2.0/linkup-2.2.0-x86_64-apple-darwin.tar.gz"
-MAC_X86_SHA = "12594be9a7019602916c2df0e271824c2dae9aac2d4fb7abcd3f9b09b26ebf6a"
+require 'open-uri'
+require 'json'
 
-LINUX_ARM_URL = "https://github.com/mentimeter/linkup/releases/download/2.2.0/linkup-2.2.0-aarch64-unknown-linux-gnu.tar.gz"
-LINUX_ARM_SHA = "f53b2a4b1ec44b0540a8ad626cf41f1ea69da3aa2e67a5886852ee9f3e01afe6"
-LINUX_X86_URL = "https://github.com/mentimeter/linkup/releases/download/2.2.0/linkup-2.2.0-x86_64-unknown-linux-gnu.tar.gz"
-LINUX_X86_SHA = "323a0acb5f97174c5d36f9e19a8edc76a3f4a658d2ed44ae546f90e130cb9a11"
+LATEST_RELEASE = JSON.parse(URI.open('https://api.github.com/repos/mentimeter/linkup/releases/latest').read)
+ASSETS = LATEST_RELEASE['assets']
 
 class Linkup < Formula
-  desc "Link up preview environments"
-  homepage "https://github.com/mentimeter/linkup"
-  license "Apache-2.0"
+  desc 'Link up preview environments'
+  homepage 'https://github.com/mentimeter/linkup'
+  license 'Apache-2.0'
+
+  depends_on 'cloudflared'
+  depends_on 'dnsmasq'
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url MAC_ARM_URL
-      sha256 MAC_ARM_SHA
+      mac_arm_asset = ASSETS.find { |asset| asset['name'].match(/darwin-aarch64\.zip$/) }
+      sha_asset = ASSETS.find { |asset| asset['name'].match(/darwin-aarch64\.zip\.sha256$/) }
+
+      url mac_arm_asset['browser_download_url']
+      sha256 URI.open(sha_asset['browser_download_url']).read.split.first
     elsif Hardware::CPU.intel?
-      url MAC_X86_URL
-      sha256 MAC_X86_SHA
+      mac_x86_asset = ASSETS.find { |asset| asset['name'].match(/darwin-x86\.zip$/) }
+      sha_asset = ASSETS.find { |asset| asset['name'].match(/darwin-x86\.zip\.sha256$/) }
+
+      url mac_x86_asset['browser_download_url']
+      sha256 URI.open(sha_asset['browser_download_url']).read.split.first
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url LINUX_ARM_URL
-      sha256 LINUX_ARM_SHA
+      linux_arm_asset = ASSETS.find { |asset| asset['name'].match(/linux-aarch64\.zip$/) }
+      sha_asset = ASSETS.find { |asset| asset['name'].match(/linux-aarch64\.zip\.sha256$/) }
+
+      url linux_arm_asset['browser_download_url']
+      sha256 URI.open(sha_asset['browser_download_url']).read.split.first
     elsif Hardware::CPU.intel?
-      url LINUX_X86_URL
-      sha256 LINUX_X86_SHA
+      linux_x86_asset = ASSETS.find { |asset| asset['name'].match(/linux-x86\.zip$/) }
+      sha_asset = ASSETS.find { |asset| asset['name'].match(/linux-x86\.zip\.sha256$/) }
+
+      url linux_x86_asset['browser_download_url']
+      sha256 URI.open(sha_asset['browser_download_url']).read.split.first
     end
   end
 
-  depends_on "cloudflared"
-  depends_on "dnsmasq"
+  livecheck do
+    url :homepage
+    regex(/^(\d+(?:\.\d+)+)$/i)
+    strategy :github_latest do |json, regex|
+      match = json['tag_name']&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
+  end
 
   def install
     bin.install 'linkup'
+    bin.install 'caddy' => 'linkup-caddy'
 
-    if OS.mac?
-      generate_completions_from_executable(bin/"linkup", "completion", shell_parameter_format: "--shell=", shells: [:bash, :zsh])
-    end
+    return unless OS.mac?
+
+    generate_completions_from_executable(bin / 'linkup', 'completion', shell_parameter_format: '--shell=',
+                                                                       shells: %i[bash zsh])
   end
 end
